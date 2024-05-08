575e87989ae1df4c5fa5ab6b337af65acba65392
mo <moiscoding@proton.me>
2024-05-08 21:25:16 +0330

    made the function that initializes the db

--- sqlite.py
+++ sqlite.py
@@ -1,6 +1,28 @@
 import sqlite3
 
-def createdb(): 
+
+def init_db():
     con = sqlite3.connect("sqlite.db")
     cur = con.cursor()
-    cur.execute("CREAT TABLE task()")
+    cur.execute("""
+                CREATE TABLE task
+                    (id INT AUTO INCREMENT,
+                     description TEXT,
+                     tag TEXT,
+                     priority TEXT,
+                     status Boolean,
+                     start_date TIMESTAMP,
+                     end_date TIMESTAMP,
+                     time_spent TIME,
+                     PRIMARY KEY(id)
+                );
+                """)
+    cur.execute(""" CREATE TABLE time
+                (id INT AUTO INCREMENT,
+                 task_id INT, 
+                 date TIMESTAMP,
+                 duration TIME,
+                 PRIMARY KEY(id),
+                 FOREIGN KEY(task_id) REFERENCES task(id)
+                ); """)
+    con.commit()
